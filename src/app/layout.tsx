504831f2cabd3ddc4a081910690a6d04f6604343
Vincent <Vincent.ahlin99@gmail.com>
2024-04-13 22:31:35 +0200

    New, layout, css, and animation utils

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,8 @@ import { PlaylistProvider } from "@/Providers/PlaylistProvider";
 import authOptions from "@/app/api/auth/[...nextauth]/authOptions";
 
 import { getServerSession } from "next-auth";
-import SignIn from "../components/SignIn";
+
+import SignIn from "@/components/SignIn";
 import "./globals.css";
 
 export default async function RootLayout({
